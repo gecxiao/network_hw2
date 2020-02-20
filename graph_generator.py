e1c54e49bfd5dc153e970c884ef448f31e135fe0
Chengxiao Ge <gecc@bc.edu>
2020-02-20 12:42:51 -0500

    Checked the clarification
    modify the code to satisfy the input requirement

--- graph_generator.py
+++ graph_generator.py
@@ -3,17 +3,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
-n = int(input("Enter number of nodes : "))
+fh=open("test.edgelist.txt",'rb')
+G=nx.read_edgelist(fh)
 
-node_list = list(map(str, input("\nEnter the nodes(put space between two nodes): ").strip().split()))[:n]
-for node in node_list:
-    n = int(input("Enter number of neighbors of " + str(node) + " :"))
-    neighbor_list = list(map(str, input("\nEnter the neighbors(put space between two nodes): ").strip().split()))[:n]
 
-for n in node_list:
-    for nb in neighbor_list:
-        G.add_edge(n, nb)
 
 #plt.draw()
 nx.draw(G)
