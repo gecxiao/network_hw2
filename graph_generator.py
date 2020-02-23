77c1e47a0a4ed7468b8e526e50c197252c0385f9
Chengxiao Ge <gecc@bc.edu>
2020-02-23 14:50:09 -0500

    Implement BFS
    Updated function of BFS

--- graph_generator.py
+++ graph_generator.py
@@ -3,13 +3,38 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
+def BFS(G, s):
+    visited={}
+    for node in nx.nodes(G):
+        visited[str(node)]=False
+    queue = []
+    S = []
+    queue.append(s)
+    visited[str(s)] = True
+    while queue:
+        s = queue.pop(0)
+        for v in G.neighbors(s):
+            if not visited[str(v)]:
+                queue.append(v)
+                visited[str(v)] = True
+                S.append(v)
+    return S
+
+
+def max_flow(G, s, t):
+    f = 0
+    G_reverse = nx.reverse(G)
+
 fh=open("test.edgelist.txt",'rb')
 G=nx.read_edgelist(fh)
-print(G.nodes.data())
-print(G.edges.data())
-
-
+#print(G.nodes.data())
+#print(G.edges.data())
+#print(G.nodes)
+print(BFS(G,'1'))
 #plt.draw()
 nx.draw(G)
 
 #has some issue with creating isolated node.
+
+
