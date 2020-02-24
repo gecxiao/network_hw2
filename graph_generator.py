b245c3344064b8356b075434d83ac97f839d8968
Chengxiao Ge <gecc@bc.edu>
2020-02-24 17:09:41 -0500

    bug fix

--- graph_generator.py
+++ graph_generator.py
@@ -18,7 +18,7 @@ def bfs(G, s):
     while queue:  # while ToExplore is non-empty
         s = queue.pop(0)  # remove s from ToExplore
         for v in G.neighbors(s):  # for every edge in adj(s)
-            if not visited[str(v)] and G[s][v]['weight']!=0:  # if (visited[y]==False)
+            if not visited[str(v)]:  # if (visited[y]==False)
                 queue.append(v)  # add y toexplore
                 visited[str(v)] = True  # set visited[y]=True
                 S.append(v)  # add y to S.
@@ -30,13 +30,13 @@ def bfs_helper(G, s, t):
     return t in S
 
 
-def ford_fulkerson(G, s, t):
+def ford_fulkerson_bfs(G, s, t):
     f = 0  # maxflow number
     path = bfs(G, s)
     for i in range(1, len(path)):
-        G.add_edge(str(i+1),str(i), weight = 0)
+        G.add_edge(path[i], path[i-1], weight=0)
     while bfs_helper(G, s, t):  # if there is a path from s to t in the residual graph
-        path = bfs(G, s) #[1,2,3,4]
+        path = bfs(G, s)  # [1,2,3,4]
         i = 0
         capacity = float('inf')
         while path[i] != t:
@@ -45,23 +45,52 @@ def ford_fulkerson(G, s, t):
         i = 0
         while path[i] != t:
             G[path[i]][path[i + 1]]['weight'] -= capacity
-            #G.add_edge(i+1,i, weight = 0)
-            #print(G.edges.data())
+            # G.add_edge(i+1,i, weight = 0)
+            # print(G.edges.data())
             G[path[i + 1]][path[i]]['weight'] += capacity
             i += 1
-            print(G.edges.data())
-            print('next')
         f += capacity
+        if capacity == 0:
+            break
     return f
+
+
+def ford_fulkerson_dijkstra(G, s, t):
+    f = 0  # maxflow number
+    path = nx.dijkstra_path(G, s, t)
+    for i in range(1, len(path)):
+        G.add_edge(path[i], path[i-1], weight=0)
+    while nx.dijkstra_path(G, s, t):  # if there is a path from s to t in the residual graph
+        path = nx.dijkstra_path(G, s, t)  # [1,2,3,4]
+        i = 0
+        capacity = float('inf')
+        while path[i] != t:
+            capacity = min(G[path[i]][path[i + 1]]['weight'], capacity)
+            i += 1
+        i = 0
+        while path[i] != t:
+            G[path[i]][path[i + 1]]['weight'] -= capacity
+            # G.add_edge(i+1,i, weight = 0)
+            # print(G.edges.data())
+            G[path[i + 1]][path[i]]['weight'] += capacity
+            i += 1
+        f += capacity
+        if capacity == 0:
+            break
+    return f
+
 
 fh = open("test.edgelist.txt", 'rb')
 G = nx.read_edgelist(fh, create_using=nx.DiGraph)
 # print(G.nodes.data())
-#print(G.edges.data())
+# print(G.edges.data())
 # print(G.nodes)
 # print(bfs(G, '1'))
 # print(G.edges())
-f=ford_fulkerson(G, '1', '2')
+#f=ford_fulkerson_bfs(G, '3', '4')
+#print(f)
+
+f = ford_fulkerson_dijkstra(G, '3', '4')
 print(f)
 # plt.draw()
 
