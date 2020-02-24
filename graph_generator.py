27c541d8d24220d16e2ce2d263f2cc51e2ca32b5
Chengxiao Ge <gecc@bc.edu>
2020-02-23 22:08:17 -0500

    Update graph_generator.py

--- graph_generator.py
+++ graph_generator.py
@@ -4,37 +4,45 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 
-def BFS(G, s):
-    visited={}
+def bfs(G, s):
+    visited = {}  # initialize visited, using dict.
     for node in nx.nodes(G):
-        visited[str(node)]=False
-    queue = []
-    S = []
-    queue.append(s)
-    visited[str(s)] = True
-    while queue:
-        s = queue.pop(0)
-        for v in G.neighbors(s):
-            if not visited[str(v)]:
-                queue.append(v)
-                visited[str(v)] = True
-                S.append(v)
+        visited[str(node)] = False  # set visited[i]=False
+    queue = []  # initialize ToExplore list, since BFS, using queue
+    S = []  # the list we want to return
+    S.append(s)  # add s to S.
+    queue.append(s)  # add s to ToExplore
+    visited[str(s)] = True  # set visitied[s] tobe true.
+    while queue:  # while ToExplore is non-empty
+        s = queue.pop(0)  # remove s from ToExplore
+        for v in G.neighbors(s):  # for every edge in adj(s)
+            if not visited[str(v)]:  # if (visited[y]==False)
+                queue.append(v)  # add y toexplore
+                visited[str(v)] = True  # set visited[y]=True
+                S.append(v)  # add y to S.
     return S
 
 
-def max_flow(G, s, t):
-    f = 0
-    G_reverse = nx.reverse(G)
+def bfs_helper(G, s, t):
+    S = bfs(G, s)
+    return t in S
 
-fh=open("test.edgelist.txt",'rb')
-G=nx.read_edgelist(fh)
-#print(G.nodes.data())
-#print(G.edges.data())
-#print(G.nodes)
-print(BFS(G,'1'))
-#plt.draw()
+
+def ford_fulkerson(G, s, t):
+    f = 0  # maxflow number
+    G_residual = nx.preflow_push(G, s, t)  #
+    while bfs_helper(G_residual, s, t):  # if there is a path from s to t in the reversed graph
+        return 1
+    return f
+
+
+fh = open("test.edgelist.txt", 'rb')
+G = nx.read_edgelist(fh)
+# print(G.nodes.data())
+# print(G.edges.data())
+# print(G.nodes)
+print(bfs(G, '1'))
+# plt.draw()
 nx.draw(G)
 
-#has some issue with creating isolated node.
-
-
+# has some issue with creating isolated node.
